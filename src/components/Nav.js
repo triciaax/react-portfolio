4b52fb49a0fa614917f2c8cc3fb1650006c5f449
Tricia <tricianghiem@gmail.com>
2022-09-19 15:33:51 -0500

    added responsiveness to nav

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -10,38 +10,19 @@ export default function Nav(props) {
         <div class="flex items-center">
           <img
             src="logo.png"
-            class="mr-3 h-6 sm:h-20"
+            class="mr-3 h-16"
             alt="Tricia Nghiem Logo"
             onClick={() => changePage("about-me")}
           />
         </div>
-        <button
-          data-collapse-toggle="navbar-default"
-          type="button"
-          class="inline-flex items-center p-2 ml-3 text-lg text-white rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 "
-          aria-controls="navbar-default"
-          aria-expanded="false"
+        <div
+          class="w-full md:block md:w-auto sm:block sm:w-auto"
+          id="navbar-default"
         >
-          <span class="sr-only">Open main menu</span>
-          <svg
-            class="w-6 h-6"
-            aria-hidden="true"
-            fill="currentColor"
-            viewBox="0 0 20 20"
-            xmlns="http://www.w3.org/2000/svg"
-          >
-            <path
-              fill-rule="evenodd"
-              d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z"
-              clip-rule="evenodd"
-            ></path>
-          </svg>
-        </button>
-        <div class="hidden w-full md:block md:w-auto" id="navbar-default">
-          <ul class=" flex flex-col p-4 mt-4 bg-gray-50 border border-gray-100 md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-medium md:border-0 md:bg-white dark:bg-white md:dark:bg-green-900">
+          <ul class=" flex flex-col p-4 mt-4 bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-medium md:border-0 dark:bg-green-900">
             <li>
               <button
-                class="block py-2 pr-4 pl-3 text-white bg-blue-700 rounded md:bg-transparent md:text-white md:p-0 dark:text-white"
+                class="block py-2 pr-4 pl-3 text-white bg-green-900 rounded md:bg-transparent md:text-white md:p-0 dark:text-white text-sm"
                 aria-current="page"
                 onClick={() => changePage("about-me")}
               >
@@ -50,7 +31,7 @@ export default function Nav(props) {
             </li>
             <li>
               <button
-                class="block py-2 pr-4 pl-3 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
+                class="block py-2 pr-4 pl-3 text-white bg-green-900 rounded md:bg-transparent md:text-white md:p-0 dark:text-white text-sm"
                 onClick={() => changePage("portfolio")}
               >
                 portfolio
@@ -58,7 +39,7 @@ export default function Nav(props) {
             </li>
             <li>
               <button
-                class="block py-2 pr-4 pl-3 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
+                class="block py-2 pr-4 pl-3 text-white bg-green-900 rounded md:bg-transparent md:text-white md:p-0 dark:text-white text-sm"
                 onClick={() => changePage("resume")}
               >
                 resume
@@ -66,7 +47,7 @@ export default function Nav(props) {
             </li>
             <li>
               <button
-                class="block py-2 pr-4 pl-3 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
+                class="block py-2 pr-4 pl-3 text-white bg-green-900 rounded md:bg-transparent md:text-white md:p-0 dark:text-white text-sm"
                 onClick={() => changePage("contact-me")}
               >
                 contact
