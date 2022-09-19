ccdf52fcd80df83a0a6ad2808bc2c71cc51e7145
Tricia <tricianghiem@gmail.com>
2022-09-19 14:20:10 -0500

    created sucess page

---
+++ src/components/body-components/ContactSuccess.js
@@ -0,0 +1,8 @@
+export default function ContactSuccess() {
+  return (
+    <div class="p-20">
+      <p class="text-2xl tracking-wide p-10"> THANK YOU FOR YOUR MESSAGE.</p>
+      <p>I will get back to you as soon as possible! </p>
+    </div>
+  );
+}
