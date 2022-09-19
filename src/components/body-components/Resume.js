b21f639deb81a291cbbb08ab06592a035347c162
Tricia <tricianghiem@gmail.com>
2022-09-18 23:12:24 -0500

    added resume html/pdf

---
+++ src/components/body-components/Resume.js
@@ -0,0 +1,110 @@
+export default function Resume() {
+  return (
+    <div class="p-20 text-left">
+      <p>
+        <b>
+          Business Product Manager - Back-end and Emerging Technology -
+          Microsoft
+        </b>
+        <br></br> July 2022 - Current
+        <br></br>
+        <ul>
+          <li>
+            -Prioritize strategic initiatives for the Backend and Emerging
+            Technology team for online experiences and Microsoft Experience
+            Centers
+          </li>
+          <li>-Plan feature optimizations and new product developments</li>
+          <li>
+            -Create documentation to support program lifecycle including
+            roadmaps and go to market strategy
+          </li>
+          <li>
+            -Develop a deep understanding of the business objectives of
+            stakeholders and users; evangelized the product vision and build
+            trust by maintaining an accurate and achievable roadmap
+          </li>
+        </ul>
+        <br></br>
+        <b>Digital Site Merchandiser - Apparel & Accessories - Target</b>
+        <br></br>December 2019 - June 2022
+        <br></br>
+        <ul>
+          <li>
+            -Collaborate with 14 buying teams, 15+ internal teams, and external
+            vendors to analyze, develop and execute monthly kick-offs and
+            special projects for Tween, Kids, Toddler, and Baby segments to
+            ensure digital experiences are planned based on Key Performance
+            Indicators (KPIs), inventory, and financial forecasts
+          </li>
+          <li>
+            -Create and execute promotional strategy on web, Target application,
+            and email growing digital industry market share from #4 to #2
+          </li>
+          <li>
+            -Report on web site and Target application content increasing
+            conversion rate by 35%
+          </li>
+          <li>
+            -Own the end-to-end digital strategy for special projects resulting
+            in 1.6M visits and $14.2M+ in sales Strategize and kick off Digital
+            Only experience for new market segment forecasted at $16M+ in sales
+          </li>
+        </ul>
+        <br></br>
+        <b>Marketing Analyst II - Email Capabilities - Bluestem Brands</b>
+        <br></br>December 2018 - November 2019
+        <br></br>
+        <ul>
+          <li>
+            -Standardize audience selection process across e-commerce platform
+            improving email audience accuracy and compliance
+          </li>
+          <li>
+            -Develop and implement (start to finish) Intake Process that
+            improved cross functional efficiency and enhanced internal team
+            planning
+          </li>
+          <li>
+            -Improve click through rate upwards of 11% by delivering relevant
+            content to matching consumer profiles Mentor and train new employees
+            to enable the execution of email/SMS marketing campaigns
+          </li>
+          <li>
+            -Successfully integrate third party SaaS to support company’s
+            personalization strategies and automation (Monetate)
+          </li>
+        </ul>
+        <br></br>
+        <b>Marketing Analyst, Email Operations - Bluestem Brands</b>
+        <br></br>August 2017 – December 2018
+        <br></br>
+        <ul>
+          <li>
+            -Develop email marketing campaign using HTML/CSS/SQL that resulted
+            in $14M+ of additional sales
+          </li>
+          <li>
+            -Conduct A/B testing through Oracle Responsys to assess different
+            content, promotional offers, and send volume
+          </li>
+          <li>
+            -Optimize credit campaigns by delivering tailored content regarding
+            customer account statuses and audience segmentation
+          </li>
+          <li>
+            -Automate credit email processes, cutting team workload by 50% and
+            improving legal compliance
+          </li>
+          <li>
+            -Collaborate and launch cross-functional email marketing Post
+            Purchase campaign that generated over $100k+ of additional sales
+          </li>
+        </ul>
+      </p>
+      <p class="p-20 text-center">
+        <a href="patricia_nghiem_2022.pdf">Click to View Entire Resume</a>.
+      </p>
+    </div>
+  );
+}
