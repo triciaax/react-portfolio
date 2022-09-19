305f9df1d219203b5c0a2fd35d64230cbd7e28fd
Tricia <tricianghiem@gmail.com>
2022-09-19 12:34:10 -0500

    added more portfolio work

--- src/components/body-components/Portfolio.js
+++ src/components/body-components/Portfolio.js
@@ -30,11 +30,12 @@ export default function Portfolio() {
           <img src="Target.png"></img>
         </a>
         <br></br>
-        <b>Target</b>
+        <b>Target Web, Mobile, & Application</b>
         <br></br>Owned strategy from homepage to PDP for all of Target's Kids,
         Toddler, & Baby experiences. Specialities: web, mobile, & application
-        strategy, internal search, SEO, email marketing, item set up,
-        personalization, promo marketing, & adobe analytics, & filtering.
+        strategy, internal search, SEO, pdp optimization, email marketing, item
+        set up, personalization, promo marketing, & adobe analytics, &
+        filtering.
       </div>
 
       <div class="saturate-50">
@@ -75,6 +76,45 @@ export default function Portfolio() {
         information to ensure that their lists are cohesive and findable. This
         was a group project. Specialities: HTML, CSS, JavaScript, handlebars,
         MySQL, and node.js.
+      </div>
+
+      <div class="saturate-50">
+        <a href="https://www.genesis-mn.com/">
+          <img src="Genesis.png"></img>
+        </a>
+        <br></br>
+        <b>Genesis Group Homes</b>
+        <br></br>
+        This website was pitched to the COO while I was an HR intern at a local
+        company. Accessibility was a big iniatitive for this experience.
+        Specialities: website wireframes, strategy, execution, 401-redirects,
+        HTML, CSS, & CMS administration.
+      </div>
+
+      <div class="saturate-50">
+        <a href="https://www.target.com/c/kids-adaptive-clothing/-/N-1laue">
+          <img src="Target-adaptive.png"></img>
+        </a>
+        <br></br>
+        <b>Adaptive Collection</b>
+        <br></br>
+        This was a digital only strategy for Target's kids, toddler, & baby
+        adaptive assortment. Key functionality of the assortment was a
+        priorities for this strategy. Specialities: item data optimization,
+        email marketing, filtering, navigation, accessibility, & SEO.
+      </div>
+
+      <div class="saturate-50">
+        <a href="https://note-taker-app-2022.herokuapp.com/">
+          <img src="Notes.png"></img>
+        </a>
+        <br></br>
+        <b>Note Taker</b>
+        <br></br>
+        This application can be used to take notes and save them. Users can add
+        a title and decription to their notes and reference them later. Users
+        can also use this app to take, add, and delete notes. Specialities:
+        JavaScript, HTML, CSS, Heroku deployment, & bootstrap.
       </div>
     </div>
   );
