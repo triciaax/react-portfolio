19d8d95a490b2b28b21a33de94d0825bc4a8311f
Tricia <tricianghiem@gmail.com>
2022-09-18 23:13:36 -0500

    added responsive design to portfolio

---
+++ src/components/body-components/Portfolio.js
@@ -0,0 +1,81 @@
+export default function Portfolio() {
+  return (
+    <div class="grid grid-cols-1 gap-6 md:grid-cols-3 p-20">
+      <div class="saturate-50">
+        <a href="https://github.com/triciaax/weather-dashboard">
+          <img src="Weather-Dashboard.png"></img>
+        </a>
+        <br></br>
+        <b>Weather Dashboard</b>
+        <br></br>This was an application that was developed to find the curent
+        and forecasted weather for specific cities. It uses local storage to
+        save previous searches. Specialities: HTML, CSS, JavaScript, bootstrap,
+        & APIs.
+      </div>
+
+      <div class="saturate-50">
+        <a href="https://triciaax.github.io/Wine-and-Dine/">
+          <img src="wine&dine.png"></img>
+        </a>
+        <br></br>
+        <b>Wine & Dine</b>
+        <br></br>This application is a recipe generator for food and cocktails.
+        From ideation to execution, this was a collaboration among other
+        developers. Specialities: HTML, CSS, JavaScript, APIs, & strategy
+        development.
+      </div>
+
+      <div class="saturate-50">
+        <a href="https://www.target.com/c/kids/-/N-xcoz4">
+          <img src="Target.png"></img>
+        </a>
+        <br></br>
+        <b>Target</b>
+        <br></br>Owned strategy from homepage to PDP for all of Target's Kids,
+        Toddler, & Baby experiences. Specialities: web, mobile, & application
+        strategy, internal search, SEO, email marketing, item set up,
+        personalization, promo marketing, & adobe analytics, & filtering.
+      </div>
+
+      <div class="saturate-50">
+        <a href="https://www.target.com/c/cat-jack-mix-and-match-collection/-/N-uuskq">
+          <img src="Target-M&M.png"></img>
+        </a>
+        <br></br>
+        <b>Gender Neutral Collection</b>
+        <br></br>
+        This was a digital only strategy for a Gender Inclusive collection for
+        Target's Cat & Jack brand across all ages. Specialities: collection item
+        data, email marketing, shoppable drawers, experimental a/b testing, &
+        item data optimization.
+      </div>
+
+      <div class="saturate-50">
+        <a href="https://github.com/triciaax/tech-blog">
+          <img src="TechBlog.png"></img>
+        </a>
+        <br></br>
+        <b>Tech Blog</b>
+        <br></br>
+        This is a CMS-style blog site similar to a users can publish, edit,
+        comment and delete their blog posts. Users can also create an account,
+        login, logout, and manage their blog posts. Specialities: express.js,
+        MySQL, html/css, handlebars, and sequelize.
+      </div>
+
+      <div class="saturate-50">
+        <a href="https://shielded-mountain-23922.herokuapp.com/login">
+          <img src="ThingsWanted.png"></img>
+        </a>
+        <br></br>
+        <b>Things Wanted</b>
+        <br></br>
+        This is an application that helps users create wishlist and find other
+        user's wishlist. Users are able to input urls, titles, and other
+        information to ensure that their lists are cohesive and findable. This
+        was a group project. Specialities: HTML, CSS, JavaScript, handlebars,
+        MySQL, and node.js.
+      </div>
+    </div>
+  );
+}
