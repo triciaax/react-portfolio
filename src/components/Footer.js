e7f96b54facaacffb660190c3d24d684504a68c8
Tricia <tricianghiem@gmail.com>
2022-09-18 23:12:35 -0500

    created footer html/css

---
+++ src/components/Footer.js
@@ -0,0 +1,9 @@
+export default function Footer() {
+  return (
+    <footer class="text-center text-white bg-green-900">
+      <div class="text-center text-white p-4">
+        © 2022 Copyright Tricia Nghiem
+      </div>
+    </footer>
+  );
+}
