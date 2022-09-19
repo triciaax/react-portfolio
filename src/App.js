6dfecc1e147017f8a8503faaf440742a90c67f2e
Tricia <tricianghiem@gmail.com>
2022-09-18 23:14:50 -0500

    added imports for components and useState

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import Nav from "./components/Nav";
+import { useState } from "react";
+import "./App.css";
+import Body from "./components/Body";
+import Footer from "./components/Footer";
 
 function App() {
+  const [activePage, setActivePage] = useState("about-me");
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Nav setActivePage={setActivePage} />
+      <Body activePage={activePage} />
+      <Footer />
     </div>
   );
 }
